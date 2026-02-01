55f8dac700542b8bd9a1f873201982a9a6e0318b
makhsodur.rahman <makhsodurrahmanhridoy@gmail.com>
2026-02-01 20:14:23 +0600

    example of java thread topic of stamplock

---
+++ src/class_17_stamplock/Main.java
@@ -0,0 +1,65 @@
+package class_17_stamplock;
+
+import java.util.concurrent.locks.StampedLock;
+
+class BankAccount {
+    private double balance;
+    private StampedLock lock = new StampedLock();
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        long stamp = lock.writeLock();
+        try {
+            balance += amount;
+            System.out.println("Deposit " + amount + " And update balance : " + balance);
+        } finally {
+            lock.unlock(stamp);
+        }
+    }
+
+    public void checkBalance() {
+        long stamp = lock.tryOptimisticRead();
+        double currentBalance = balance;
+
+        Main.sleep(500);
+
+        if (!lock.validate(stamp)) {
+            stamp = lock.readLock();
+            try {
+                currentBalance = balance;
+            } finally {
+                lock.unlock(stamp);
+            }
+
+        }
+
+
+        System.out.println("Current balance :: " + currentBalance);
+    }
+
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        BankAccount bankAccount = new BankAccount(1000);
+
+        new Thread(bankAccount::checkBalance).start();
+        new Thread(() -> {
+            bankAccount.deposit(500);
+        }).start();
+    }
+
+    public static void sleep(int i) {
+
+        try {
+            Thread.sleep(i);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
