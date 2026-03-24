0acf46cab890921bfa6d16069d4de4ce6868d0c0
makhsodur.rahman <makhsodur.rahman@bedatasolutions.com>
2026-03-24 14:24:02 +0600

    Add of  thread factory and  rejection handler and some example

--- src/class_28_executor_service/Main.java
+++ src/class_28_executor_service/Main.java
@@ -5,37 +5,54 @@ import utils.Utils;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
+class MakRejectionHandler implements RejectedExecutionHandler {
+
+    ThreadPoolExecutor differentExecutor = new ThreadPoolExecutor(
+            5,
+            5, 10
+            , TimeUnit.SECONDS,
+            new ArrayBlockingQueue<>(10));
+
+    @Override
+    public void rejectedExecution(Runnable r, ThreadPoolExecutor e) {
+        differentExecutor.execute(r);
+        //TODO
+        //When use this real project store this task in db or other and after some time exec or run any other operation
+    }
+}
+
+class MakThreadFactory implements ThreadFactory {
+    AtomicInteger count = new AtomicInteger(1);
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(r);
+        t.setName("Worker-thread-" + count.getAndIncrement());
+        t.setDaemon(true);
+        return t;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        AtomicInteger count = new AtomicInteger(1);
+
 
         ThreadPoolExecutor executor = new ThreadPoolExecutor(
                 5,
                 5, 10
                 , TimeUnit.SECONDS,
                 new ArrayBlockingQueue<>(10),
-                r -> {
-                    Thread t = new Thread(r);
-                    t.setName("Worker-thread-" + count.getAndIncrement());
-                    t.setDaemon(true);
-                    return t;
-                });
+                new MakThreadFactory(),
+                new MakRejectionHandler()
+        );
 
 
+        for (int i = 0; i < 16; i++) {
+            int finalI = i;
+            executor.execute(() -> {
+                System.out.println("task id :: " + finalI+" exec by : "+ Thread.currentThread().getName());
+            });
 
-        executor.execute(() -> {
-            for (int i = 0; i < 10; i++) {
-                Utils.sleep(1000);
-                System.out.println(Thread.currentThread().getName()+ " :: "+i);
-            }
-        });
-
-        executor.execute(() -> {
-            for (int i = 0; i < 10; i++) {
-                Utils.sleep(1000);
-                System.out.println(Thread.currentThread().getName()+ " :: "+i);
-            }
-        });
+        }
 
 
         Utils.sleep(5000);
